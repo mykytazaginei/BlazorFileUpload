ce8eede0ee47bc0002bd3f223fe2e4c4998a7329
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-10-18 14:52:13 +0200

    Implement the Controller

---
+++ Server/Controllers/FIleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using BlazorFileUpload.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFileUpload.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FIleController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public FIleController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UploadResult>>> UploadFile(List<IFormFile> files)
+        {
+
+            List<UploadResult> uploadResults = new List<UploadResult>();
+             
+            foreach (var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.FileName;
+                uploadResult.FileName = untrustedFileName;
+                var trustedFileNameForDisplay = WebUtility.HtmlDecode(untrustedFileName);
+
+                trustedFileNameForFileStorage = Path.GetRandomFileName();
+                var path = Path.Combine(_env.ContentRootPath, "uploads", trustedFileNameForFileStorage);
+
+                await using FileStream fs = new FileStream(path, FileMode.Open);
+                await fs.CopyToAsync(fs);
+
+                uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                uploadResults.Add(uploadResult);
+
+            }
+
+            return Ok(uploadResults);
+
+        }
+    }
+}
